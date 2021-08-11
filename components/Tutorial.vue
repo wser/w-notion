e5c52edec17ba76dc3aab0d600aedee2ee02004a
wser <w.web.dev@gmail.com>
2021-08-11 07:46:09 +0200

    added vite

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -16,6 +16,7 @@
         <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
           To get started, remove <code class="bg-gray-100 text-sm p-1 rounded border">components/Tutorial.vue</code> and start coding in <code class="bg-gray-100 text-sm p-1 rounded border">pages/index.vue</code>. Have fun!
         </p>
+        <p>Hello w</p>
       </div>
       <div class="flex justify-center pt-4 space-x-2">
         <a href="https://github.com/nuxt/nuxt.js" target="_blank"><svg
